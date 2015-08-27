22f6982f884c17ba7bf265dc1eb2bc87173c99b4
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-27 20:39:24 -0300

    [azk] webapp is a build step

--- Azkfile.js
+++ Azkfile.js
@@ -1,6 +1,6 @@
 systems({
   "cloudtunes-webapp": {
-    depends: [],
+    depends: ["cloudtunes-server"],
     image: {"docker": "azukiapp/ruby"},
     provision: [
       "npm install",
@@ -9,8 +9,7 @@ systems({
     ],
     workdir: "/azk/#{system.name}",
     shell: "/bin/bash",
-    command: "node_modules/.bin/http-server build/",
-    wait: 20,
+    wait: undefined,
     mounts: {
       '/azk/#{system.name}': path("./cloudtunes-webapp"),
 
@@ -26,18 +25,98 @@ systems({
       "/azk/#{system.name}/.bundle": path("#{system.name}/.bundle")
     },
     scalable: {"default": 1},
-    http: {
-      domains: [
-        "#{system.name}.#{azk.default_domain}", // default azk
-        "#{process.env.AZK_HOST_IP}"            // used if deployed
-      ]
-    },
-    ports: {
-      http: "8000/tcp"
-    },
+    http: null,
+    ports: null,
     envs: {
-      NODE_ENV: "production",
-      PORT: "8000",
+      NODE_ENV: "development"
     },
   },
+
+  "cloudtunes-server": {
+    depends: ['mongo', 'redis'],
+    image: {"docker": "azukiapp/python:2.7.9"},
+    provision: [
+      "pip install --user --allow-all-external -r requirements.txt",
+      // "pip install -e .",
+      // "cp cloudtunes/settings/local.example.py cloudtunes/settings/local.py",
+    ],
+    workdir: "/azk/#{manifest.dir}",
+    shell: "/bin/bash",
+    mounts  : {
+      '/azk/#{manifest.dir}': path('./cloudtunes-server'),
+      '/azk/pythonuserbase':  persistent('pythonuserbase'),
+    },
+    scalable: {"default": 1},
+
+    command: [
+      "pip install -e .",
+      // "cloudtunes-worker worker --loglevel=INFO -c 4 &",
+      "cloudtunes-server"
+    ].join(";"),
+
+    wait: {"retry": 20, "timeout": 1000},
+    http: {
+      domains: [ "#{system.name}.#{azk.default_domain}" ]
+    },
+    ports: {
+      http: "8001/tcp",
+      flash: "10843/tcp",
+    },
+    envs: {
+      // set instances variables
+      PATH : '/azk/pythonuserbase/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      PYTHON_ENV : 'development',
+      PYTHONUSERBASE: '/azk/pythonuserbase',
+    }
+  },
+
+  "cloudtunes-server-worker": {
+    extends: 'cloudtunes-server',
+    command: [
+      "pip install -e .",
+      "cloudtunes-worker worker --loglevel=INFO -c 4"
+    ].join(";"),
+
+    wait: undefined,
+    http: null,
+    ports: null,
+  },
+
+  redis: {
+    image: { docker: "redis" },
+    scalable: { default: 1, limit: 1 },
+    http      : {
+      domains: [ "#{system.name}.#{azk.default_domain}" ],
+    },
+    ports: {
+      http: "6379:6379/tcp",
+    },
+    export_envs: {
+      REDIS_URL: "redis://#{net.host}:#{net.port[6379]}"
+    }
+  },
+
+  mongo: {
+    image : { docker: "azukiapp/mongodb" },
+    scalable: false,
+    wait: { retry: 60, timeout: 1000 },
+    // Mounts folders to assigned paths
+    mounts: {
+      // equivalent persistent_folders
+      '/data/db': persistent('mongodb-#{manifest.dir}'),
+    },
+    ports: {
+      http: "28017/tcp",
+      data: "27017:27017/tcp",
+    },
+    http      : {
+      domains: [ "#{system.name}.#{azk.default_domain}" ],
+    },
+    export_envs        : {
+      MONGODB_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_development",
+      // LCB_DATABASE_URI: "mongodb://mongo/letschat",
+      LCB_DATABASE_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_development",
+    },
+  },
+
 });
