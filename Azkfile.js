2c6dc7389a9d6ae346da7a5f428cd255dc7e929d
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-28 17:26:29 -0300

    [azk] Getting info from ENV

--- Azkfile.js
+++ Azkfile.js
@@ -1,56 +1,23 @@
 systems({
-  "cloudtunes-webapp": {
-    depends: ["cloudtunes-server"],
-    image: {"docker": "azukiapp/ruby"},
+  "cloudtunes-server": {
+    depends: ["mongo", "redis", "cloudtunes-webapp"],
+    image: {"docker": "azukiapp/python:2.7.9"},
     provision: [
-      "npm install",
-      "bundle install --path /azk/bundler",
-      "node_modules/.bin/brunch build --env config-dist.coffee",
+      "cd /azk/#{system.name}/cloudtunes-server/ && pip install --user --allow-all-external -r requirements.txt",
+      "cp /azk/#{system.name}/cloudtunes-server/cloudtunes/settings/local.example.py"
+      + " /azk/#{system.name}/cloudtunes-server/cloudtunes/settings/local.py",
     ],
     workdir: "/azk/#{system.name}",
     shell: "/bin/bash",
-    wait: undefined,
-    mounts: {
-      '/azk/#{system.name}': path("./cloudtunes-webapp"),
-
-      // node
-      '/azk/#{system.name}/node_modules': persistent("./node_modules"),
-      '/azk/#{system.name}/.sass-cache': persistent("./.sass-cache"),
-      '/azk/#{system.name}/build': persistent("./build"),
-
-      // ruby
-      "/azk/bundler": persistent("#{system.name}/bundler"),
-      "/azk/#{system.name}/tmp": persistent("#{system.name}/tmp"),
-      "/azk/#{system.name}/log": path("#{system.name}/log"),
-      "/azk/#{system.name}/.bundle": path("#{system.name}/.bundle")
-    },
-    scalable: {"default": 1},
-    http: null,
-    ports: null,
-    envs: {
-      NODE_ENV: "development"
-    },
-  },
-
-  "cloudtunes-server": {
-    depends: ['mongo', 'redis'],
-    image: {"docker": "azukiapp/python:2.7.9"},
-    provision: [
-      "pip install --user --allow-all-external -r requirements.txt",
-      // "pip install -e .",
-      // "cp cloudtunes/settings/local.example.py cloudtunes/settings/local.py",
-    ],
-    workdir: "/azk/#{manifest.dir}",
-    shell: "/bin/bash",
     mounts  : {
-      '/azk/#{manifest.dir}': path('./cloudtunes-server'),
-      '/azk/pythonuserbase':  persistent('pythonuserbase'),
+      "/azk/#{system.name}": path("."),
+      "/azk/pythonuserbase":  persistent("pythonuserbase"),
     },
     scalable: {"default": 1},
 
     command: [
+      "cd cloudtunes-server",
       "pip install -e .",
-      // "cloudtunes-worker worker --loglevel=INFO -c 4 &",
       "cloudtunes-server"
     ].join(";"),
 
@@ -64,15 +31,20 @@ systems({
     },
     envs: {
       // set instances variables
-      PATH : '/azk/pythonuserbase/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      PYTHON_ENV : 'development',
-      PYTHONUSERBASE: '/azk/pythonuserbase',
+      PATH : "/azk/pythonuserbase/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+      PYTHON_ENV : "production",
+      PYTHONUSERBASE: "/azk/pythonuserbase",
+      //
+      // edit this ENVS on .env
+      // $ cp .env.example .env
+      //
     }
   },
 
   "cloudtunes-server-worker": {
-    extends: 'cloudtunes-server',
+    extends: "cloudtunes-server",
     command: [
+      "cd cloudtunes-server",
       "pip install -e .",
       "cloudtunes-worker worker --loglevel=INFO -c 4"
     ].join(";"),
@@ -80,6 +52,39 @@ systems({
     wait: undefined,
     http: null,
     ports: null,
+  },
+
+  "cloudtunes-webapp": {
+    depends: [],
+    image: {"docker": "azukiapp/ruby"},
+    provision: [
+      "npm install",
+      "bundle install --path /azk/bundler",
+      "node_modules/.bin/brunch build --env config-dist.coffee",
+    ],
+    workdir: "/azk/#{system.name}",
+    shell: "/bin/bash",
+    wait: undefined,
+    mounts: {
+      "/azk/#{system.name}": path("./cloudtunes-webapp"),
+
+      // node
+      "/azk/#{system.name}/node_modules": persistent("./node_modules"),
+      "/azk/#{system.name}/.sass-cache": persistent("./.sass-cache"),
+      "/azk/#{system.name}/build": persistent("./build"),
+
+      // ruby
+      "/azk/bundler": persistent("#{system.name}/bundler"),
+      "/azk/#{system.name}/tmp": persistent("#{system.name}/tmp"),
+      "/azk/#{system.name}/log": path("#{system.name}/log"),
+      "/azk/#{system.name}/.bundle": path("#{system.name}/.bundle")
+    },
+    scalable: {"default": 1},
+    http: null,
+    ports: null,
+    envs: {
+      NODE_ENV: "production"
+    },
   },
 
   redis: {
@@ -103,7 +108,7 @@ systems({
     // Mounts folders to assigned paths
     mounts: {
       // equivalent persistent_folders
-      '/data/db': persistent('mongodb-#{manifest.dir}'),
+      "/data/db": persistent("mongodb-#{manifest.dir}"),
     },
     ports: {
       http: "28017/tcp",
@@ -113,9 +118,9 @@ systems({
       domains: [ "#{system.name}.#{azk.default_domain}" ],
     },
     export_envs        : {
-      MONGODB_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_development",
+      MONGODB_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_production",
       // LCB_DATABASE_URI: "mongodb://mongo/letschat",
-      LCB_DATABASE_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_development",
+      LCB_DATABASE_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_production",
     },
   },
 
--- cloudtunes-server/cloudtunes/settings/local.example.py
+++ cloudtunes-server/cloudtunes/settings/local.example.py
@@ -1,55 +1,52 @@
+import os
 from os import path
 
 from .defaults import *
 
-
-DEBUG = False
-
+DEBUG = True
 
 MONGODB = {
-    'host': 'localhost'
+    'host': os.getenv('MONGO_HTTP_HOST', 'localhost')
 }
-
 
 REDIS = {
-    'host': 'localhost'
+    'host': os.getenv('REDIS_HTTP_HOST', 'localhost')
 }
-
 
 WEB_APP_DIR = path.realpath(ROOT + '/../cloudtunes-webapp/build/production')
 
 
 TORNADO_APP.update({
-    'static_path': WEB_APP_DIR,
-    'debug': DEBUG,
-    'cookie_secret': 'PLEASECHANGETHIS'
+    'static_path': os.getenv('TORNADO_STATIC_PATH', WEB_APP_DIR),
+    'debug': os.getenv('TORNADO_DEBUG', DEBUG),
+    'cookie_secret': os.getenv('TORNADO_COOKIE_SECRET', 'PLEASECHANGETHIS')
 })
 
 
 EMAIL = {
-    'SMTP_SERVER': 'smtp.gmail.com',
-    'SMTP_PORT': 587,
-    'SENDER': None,
-    'PASSWORD': None
+    'SMTP_SERVER': os.getenv('SMTP_SERVER', 'smtp.gmail.com'),
+    'SMTP_PORT': os.getenv('SMTP_PORT', 587),
+    'SENDER': os.getenv('SENDER', None),
+    'PASSWORD': os.getenv('PASSWORD', None)
 }
 
 #############################################################
 # Facebook <https://developers.facebook.com/apps>
 #############################################################
 
-FACEBOOK_APP_ID = None
-FACEBOOK_APP_SECRET = None
+FACEBOOK_APP_ID = os.getenv('FACEBOOK_APP_ID', None)
+FACEBOOK_APP_SECRET = os.getenv('FACEBOOK_APP_SECRET', None)
 
 #############################################################
 # Last.fm <http://www.last.fm/api/account>
 #############################################################
 
-LASTFM_API_KEY = None
-LASTFM_API_SECRET = None
+LASTFM_API_KEY = os.getenv('LASTFM_API_KEY', None)
+LASTFM_API_SECRET = os.getenv('LASTFM_API_SECRET', None)
 
 #############################################################
 # Dropbox <https://www.dropbox.com/developers>
 #############################################################
 
-DROPBOX_API_APP_KEY = None
-DROPBOX_API_APP_SECRET = None
+DROPBOX_API_APP_KEY = os.getenv('DROPBOX_API_APP_KEY', None)
+DROPBOX_API_APP_SECRET = os.getenv('DROPBOX_API_APP_SECRET', None)
